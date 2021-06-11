21c09a3e9fccc81667ef0ab2604c45003f8392c3
Kyle <kjfossman@gmail.com>
2021-06-11 15:31:00 -0800

    commit tree d1532f95f3796ad9d95d03374760d3a3ae0d1461

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,6 +4,8 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
+      <h1>Actors Page</h1>
+      {actors.map(actor => <div className="actor">{actor.name}<ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul></div>)}
       {/*{code here}*/}
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,34 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink
+      to="/"
+      exact
+      style={{width: '100px'}}
+      activeStyle={{background: 'green'}}
+      >Home</NavLink>
+
+      <NavLink
+      to="/movies"
+      exact
+      style={{width: '100px'}}
+      activeStyle={{background: 'green'}}
+      >Movies</NavLink>
+
+      <NavLink
+      to="/directors"
+      exact
+      style={{width: '100px'}}
+      activeStyle={{background: 'green'}}
+      >Directors</NavLink>
+
+      <NavLink
+      to="/actors"
+      exact
+      style={{width: '100px'}}
+      activeStyle={{background: 'green'}}
+      >Actors</NavLink>
     </div>
   );
 };
